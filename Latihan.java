ef2e2b539e7a53b8a427c0a5996c48e93b63bbd9
Arya430 <aryardwn@gmail.com>
2021-01-28 15:55:43 +0700

    Penambahan isi

---
+++ Latihan.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+public class Latihan {
+    
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[]args){
+        String nama = "Arya Ridwan Arsy Qalam & Martua Anggasana";
+        String alamat = "Base Cibinong";
+        String kelas = "3SC2";
+        int umur = 19;
+        
+    System.out.println("Nama :" + nama + "\nAlamat :" + alamat + "\nKelas :" + kelas + "\nUmur :" + umur);
+}
+}
+
+Aryaaaaa
